8416db55cb2c3b491f5193f4f317e6d2ca81a733
Ashik Meeran Mohideen <ashikmeeranmohideen@gmail.com>
2021-10-23 07:54:11 +0530

    rendering details completed

--- app/src/main/java/com/example/git_details/MainActivity.java
+++ app/src/main/java/com/example/git_details/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.git_details;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -30,47 +31,47 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button) findViewById(R.id.button);
         EditText username = (EditText) findViewById(R.id.username);
         TextView error = (TextView) findViewById(R.id.data);
-
+        TextView user = (TextView) findViewById(R.id.user);
+        TextView followers = (TextView) findViewById(R.id.follower_cnt);
+        TextView following = (TextView) findViewById(R.id.following_cnt);
+        TextView bio = (TextView) findViewById(R.id.bio);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 response = "";
                 System.out.println("Clicked");
-//                if(username.getText() != null) {
-                Thread thread = new Thread(new Runnable() {
+                if(username.getText() != null) {
+                    Thread thread = new Thread(new Runnable() {
 
-                    @Override
-                    public void run() {
-                        try {
-                            URL url = new URL("https://api.github.com/users/" + username.getText().toString());
-                            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                            conn.setRequestMethod("GET");
-
-                            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-                            String line;
-                            while ((line = in.readLine()) != null)
-                                response += line;
-                            JSONObject obj = new JSONObject(response);
-                            String followers = obj.getString("followers");
-                            error.setText("Followers: " + followers);
-                        } catch (Exception e) {
-                            e.printStackTrace();
+                        @Override
+                        public void run() {
+                            try {
+                                URL url = new URL("https://api.github.com/users/" + username.getText().toString());
+                                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                                conn.setRequestMethod("GET");
+                                if(conn.getResponseCode() != 200)
+                                    error.setText("Invalid username");
+                                else {
+                                    BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                                    String line;
+                                    while ((line = in.readLine()) != null)
+                                        response += line;
+                                    JSONObject obj = new JSONObject(response);
+                                    user.setText(obj.getString("login"));
+                                    followers.setText(obj.getString("followers"));
+                                    following.setText(obj.getString("following"));
+                                    bio.setText(obj.getString("bio"));
+                                }
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
-                    }
-                });
-                thread.start();
+                    });
+                    thread.start();
 
-
-//                }
+                }
             }
         });
-
     }
-
-
-
-//    public void get_details(View view) {
-//        error.setText(username.getText());
-
 }
